fe7c2b0f706ee4d096c544f8146de3d177ebc1d2
vflorezz <vflz.625@gmail.com>
2025-11-08 11:58:14 -0500

    Crear RepositorioPaciente

---
+++ ClinicaApp/src/main/java/com/mycompany/clinicaapp/Persistencia/RepositorioPaciente.java
@@ -0,0 +1,14 @@
+package com.mycompany.clinicaapp.Persistencia;
+import java.io.*;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
+import com.mycompany.clinicaapp.Interfaces.IRepositorioPaciente;
+import com.mycompany.clinicaapp.Modelos.Paciente;
+
+public class RepositorioPaciente {
+    
+}
